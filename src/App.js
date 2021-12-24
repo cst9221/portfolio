f19cdeb672285be2acb83ffab61354a7425f0c36
Insub Lee <insublee@Insubs-MacBook-Pro.local>
2021-12-24 23:29:35 +0900

    기본 구조 생성

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>최승태 포트폴리오 헤더</h1>
+      </header>
+
+      <nav>
+        <ol>
+          <li>home</li>
+        </ol>
+      </nav>
+      
+      <main>
+        <section>
+          <h2>Web</h2>
+
+        </section>
+        <section>
+          <h2>Application</h2>
+
+        </section>
+        <section>
+          <h2>other</h2>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
