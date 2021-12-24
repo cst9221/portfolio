63ce7ca5f2fa7771f659f661f40f17a52ebcff62
cst9221 <chltmdxo3@gmail.com>
2021-12-24 23:35:47 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>최승태 포트폴리오</h1>
-      </header>
-
-      <nav>
-        <ol>
-          <li>home</li>
-        </ol>
-      </nav>
-
-      <main>
-        <section>
-          <h2>Web</h2>
-
-        </section>
-        <section>
-          <h2>Application</h2>
-
-        </section>
-        <section>
-          <h2>other</h2>
-        </section>
-      </main>
+      
     </div>
   );
 }
